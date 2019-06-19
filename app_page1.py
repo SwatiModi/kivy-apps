36c80a20fb19913db56fea57e78a1e2ce6e4e68a
Swati Modi <swatimodi1609@gmail.com>
2019-06-19 10:12:21 +0530

    getting default data from prev_data.txt

--- app_page1.py
+++ app_page1.py
@@ -3,6 +3,7 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+import os
 
 class ConnectPage(GridLayout):
 	def __init__(self,**kwargs):
@@ -10,16 +11,31 @@ class ConnectPage(GridLayout):
 
 		self.cols = 2
 
+		if os.path.isfile('prev_details.txt'):
+			with open('prev_details.txt','r') as f:
+				data = f.read().split(',')
+
+				prev_ip = data[0]
+				prev_port = data[1]
+				prev_username = data[2]
+
+		else :
+
+				prev_ip = ''
+				prev_port = ''
+				prev_username = ''
+
+
 		self.add_widget(Label(text = "IP : "))
-		self.ip = TextInput(multiline = True)
+		self.ip = TextInput(text = prev_ip,multiline = True)
 		self.add_widget(self.ip)
 
 		self.add_widget(Label(text = "PORT : "))
-		self.port = TextInput(multiline = True)
+		self.port = TextInput(text = prev_port,multiline = True)
 		self.add_widget(self.port)
 
 		self.add_widget(Label(text = "Username : "))
-		self.username = TextInput(multiline = True)
+		self.username = TextInput(text = prev_username,multiline = True)
 		self.add_widget(self.username)
 
 		self.submit = Button(text = 'Submit')
@@ -33,7 +49,7 @@ class ConnectPage(GridLayout):
 		username = self.username.text
 		print(f'IP is {ip}, PORT is {port} for {username}') 
 
-		with open('prev_details.text','w') as f:
+		with open('prev_details.txt','w') as f:
 			f.write(f'{ip},{port},{username}')
 
 class EpicApp(App):
